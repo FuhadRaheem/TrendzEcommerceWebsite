ea3192575bb2bd47f3ee514c15959896d1fa994c
irewumi <ayindeolasunkanmi@yahoo.com>
2020-08-23 14:51:22 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function openmenu() {
+    document.getElementById("side-menu").style.display="block";
+    document.getElementById("menu-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+}
+
+function closemenu() {
+    document.getElementById("side-menu").style.display="none";
+    document.getElementById("menu-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+}
